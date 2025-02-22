3fdd3006e8e700391807ad331fc6965aea170dcd
benyovszky <molbence@gmail.com>
2025-02-22 23:38:47 +0100

    Version bump

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'profilefield_textregex'; // Full name of the plugin (used for diagnostics).
-$plugin->version   = 2025020400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025022200;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051718;        // Requires this Moodle version.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
